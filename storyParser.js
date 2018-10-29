2ed9984f63630355b0e57266494b046e81e196a7
unknown <nicolasgauvin7@gmail.com>
2018-10-29 16:08:33 +0100

    setting up story html for future parsing

--- storyParser.js
+++ storyParser.js
@@ -1,1 +1,12 @@
-console.log("Connected to storyParser");
+function setUpStoryHtml(storyPath){
+  var xhr= new XMLHttpRequest();
+  xhr.open('GET', storyPath, true);
+  xhr.onreadystatechange= function() {
+      if (this.readyState!==4) return;
+      if (this.status!==200) return;
+      document.querySelector("#story").innerHTML = this.responseText;
+  };
+  xhr.send();
+}
+
+setUpStoryHtml('./Stories/story1.html');
