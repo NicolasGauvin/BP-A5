8156b731ddd94b8dc108b648874686290513079a
unknown <nicolasgauvin7@gmail.com>
2018-12-04 09:03:34 +0100

    improved comments and variable names

--- storyParser.js
+++ storyParser.js
@@ -8,12 +8,14 @@ var queue = [];
 
 //transforming the HTML of the story into an exploitable queue
 function parseStory(){
-  //going through all elements of the story's html
   var storyHtmlChildren = document.querySelector("#story").children;
+  //going through all elements of the story's html to find the right element to add to the queue
   for (var a = 0 ; a < storyHtmlChildren.length ; a++){
     var action = {};
     var selectedNode;
-    //if the element is variable find the relevant option that correspond to the relevant value
+    //if the element is variable find the option that correspond to the relevant value
+    //the "criterion" in the "variable" element decides what variable we are looking for (i.e. "character")
+    //we are then looking for a child element that has the same value as the global variable in its "criterion"
     if(storyHtmlChildren[a].nodeName == "VARIABLE"){
       for (var i = 0 ; i < storyHtmlChildren[a].children.length ; i++){
         if(storyHtmlChildren[a].children[i].getAttribute("criterion") == window[storyHtmlChildren[a].getAttribute("criterion")]){
@@ -27,7 +29,7 @@ function parseStory(){
       action.type = storyHtmlChildren[a].nodeName;
       selectedNode = storyHtmlChildren[a];
     }
-    //get all attributes (exept the criterion) and push it to the queue
+    //get all attributes from the relevant node (exept the criterion) and push the result it to the queue
     for (var j = 0; j < selectedNode.attributes.length; j++){
       if(selectedNode.attributes[j].name !== "criterion"){
         action[selectedNode.attributes[j].name] = selectedNode.attributes[j].value;
