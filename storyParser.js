2f9cc62bba67efb47005e451f84d310a46b172b8
unknown <nicolasgauvin7@gmail.com>
2018-12-12 16:31:43 +0100

    fixing format issues from webstorm

--- storyParser.js
+++ storyParser.js
@@ -4,45 +4,45 @@ var gender = "m";
 var context = "jungle";
 
 //list of actions to do
-var queue = [];
+const queue = [];
 
 //transforming the HTML of the story into an exploitable queue
 function parseStory(){
-  var storyHtmlChildren = document.querySelector("#story").children;
-  //going through all elements of the story's html to find the right element to add to the queue
-  for (var a = 0 ; a < storyHtmlChildren.length ; a++){
-    var action = {};
-    var selectedNode;
-    //if the element is variable find the option that correspond to the relevant value
-    //the "criterion" in the "variable" element decides what variable we are looking for (i.e. "character")
-    //we are then looking for a child element that has the same value as the global variable in its "criterion"
-    if(storyHtmlChildren[a].nodeName == "VARIABLE"){
-      for (var i = 0 ; i < storyHtmlChildren[a].children.length ; i++){
-        if(storyHtmlChildren[a].children[i].getAttribute("criterion") == window[storyHtmlChildren[a].getAttribute("criterion")]){
-          action.type = storyHtmlChildren[a].children[i].nodeName;
-          selectedNode = storyHtmlChildren[a].children[i];
+	const storyHtmlList = document.querySelector("#story").children;
+	//going through all elements of the story's html to find the right element to add to the queue
+  for (let a = 0 ; a < storyHtmlList.length ; a++){
+    let queueAction = {};
+	  let selectedElement;
+	  //if the element is variable find the option that correspond to the relevant value
+    if(storyHtmlList[a].nodeName === "VARIABLE"){
+      for (let i = 0 ; i < storyHtmlList[a].children.length ; i++){
+        //the "criterion" in the "variable" element decides what variable we are looking for (i.e. "character")
+        //we are then looking for a child element that has the same value as the global variable in its "criterion"
+        if(storyHtmlList[a].children[i].getAttribute("criterion") === window[storyHtmlList[a].getAttribute("criterion")]){
+          queueAction.type = storyHtmlList[a].children[i].nodeName;
+          selectedElement = storyHtmlList[a].children[i];
         }
       }
     }
     //element that is not variable
     else{
-      action.type = storyHtmlChildren[a].nodeName;
-      selectedNode = storyHtmlChildren[a];
+      queueAction.type = storyHtmlList[a].nodeName;
+      selectedElement = storyHtmlList[a];
     }
     //get all attributes from the relevant node (exept the criterion) and push the result it to the queue
-    for (var j = 0; j < selectedNode.attributes.length; j++){
-      if(selectedNode.attributes[j].name !== "criterion"){
-        action[selectedNode.attributes[j].name] = selectedNode.attributes[j].value;
+    for (let j = 0; j < selectedElement.attributes.length; j++){
+      if(selectedElement.attributes[j].name !== "criterion"){
+        queueAction[selectedElement.attributes[j].name] = selectedElement.attributes[j].value;
       }
     }
-    queue.push(action);
+    queue.push(queueAction);
   }
   console.log(queue)
 }
 
 //get the html from the story and puts it on the page
 function setUpStoryHtml(storyPath){
-  var xhr= new XMLHttpRequest();
+  const xhr= new XMLHttpRequest();
   xhr.open('GET', storyPath, true);
   xhr.onreadystatechange= function() {
       if (this.readyState!==4) return;
