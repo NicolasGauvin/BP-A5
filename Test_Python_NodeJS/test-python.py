0cccd276978a43184e53b68cf2f42d6a5176de45
unknown <nicolasgauvin7@gmail.com>
2019-04-08 16:03:04 +0200

    transforming the code into a NodeJS app

--- Test_Python_NodeJS/test-python.py
+++
@@ -1,7 +0,0 @@
-import time
-nexttime = time.time()
-
-i = 1
-while True:
-  print(i)
-  i += 1
---
+++ milo/public/javascript/main.js
@@ -0,0 +1,38 @@
+let queue;
+let currentScene = 0;
+
+//get the html from the story and puts it on the page
+function setUpStoryHtml(storyPath){
+	const xhr= new XMLHttpRequest();
+	xhr.open('GET', storyPath, true);
+	xhr.onreadystatechange= function() {
+		if (this.readyState!==4) return;
+		if (this.status!==200) return;
+		document.querySelector("#story").innerHTML = this.responseText;
+		queue = parseStory();
+		setUpScene();
+	};
+	xhr.send();
+}
+
+function setUpScene(){
+	clearScene();
+	currentScene++;
+	handleDrawings(queue.drawingQueue[currentScene - 1]);
+}
+
+function setSettings(){
+	settings.character = document.getElementById("character").value;
+	settings.context = document.getElementById("context").value;
+}
+
+function startStory(){
+	currentScene = 0;
+	setSettings();
+	setUpStoryHtml(settings.storyPath);
+}
+
+var socket = io.connect('http://localhost');
+socket.on('button_information', function (data) {
+	console.log(data);
+});
