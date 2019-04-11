51b872db6f1ca6914bde44479f365e2efca54507
unknown <nicolasgauvin7@gmail.com>
2019-04-11 14:52:32 +0200

    finish overlapping canvases
    detection of click on images / clear drawing / clear scene

--- milo/public/javascript/drawings.js
+++ milo/public/javascript/drawings.js
@@ -1,45 +1,46 @@
-
-	//map of the position of clickable elements in the canvas, used for click detection
-	const clickableElements = [],
-	//list of canvases for every image
-	canvasList = [],
-	canvasesHolder = document.querySelector("#canvasesHolder");
+let drawingList = [];
+const canvasesHolder = document.querySelector("#canvasesHolder"),
+	canvasClickListener = document.querySelector("#canvasClickListener"),
+	canvasX = canvasClickListener.offsetLeft,
+	canvasY = canvasClickListener.offsetTop;
 
 //Cleaning the canvas between scenes
 function clearScene(){
-	//context.clearRect(0, 0, canvas.width, canvas.height);
+	for (let i =0;i < drawingList.length; i++){
+		clearDrawing(i);
+	}
+	drawingList = [];
+}
+
+function clearDrawing(index){
+	drawingList[index].context.clearRect(0, 0, drawingList[index].canvas.width, drawingList[index].canvas.height);
 }
 
 //draw all drawings from a scene
 function handleDrawings(drawings){
 	for (let a =0;a < drawings.length; a++){
-		addDrawing(drawings[a],a);
+		addDrawing(drawings[a], a);
 	}
-	console.log(drawings)
 }
 
 //add a drawing to the canvas
 function addDrawing(drawing, index){
-	let base_image = new Image();
+	const base_image = new Image();
 	base_image.src = "." + drawing.srcfile;
+	const canvas = document.createElement('canvas'),
+		context = canvas.getContext("2d");
+	canvas.width = 1000;
+	canvas.height = 500;
+	canvas.style.zIndex = index;
+	canvasesHolder.appendChild(canvas);
 	base_image.onload = function(){
-		const canvas = document.createElement('canvas'),
-			context = canvas.getContext("2d");
-		canvas.width = 1000;
-		canvas.height = 500;
-		canvas.style.zIndex = index;
-		canvasesHolder.appendChild(canvas);
 		context.drawImage(base_image, drawing.x, drawing.y, drawing.width, drawing.height);
 	};
-	//if the drawing is set as clickable, adds it to the list
-	if (drawing.clickable === "1"){
-		clickableElements.push(drawing);
-	}
+	drawingList.push({"drawing":drawing,"canvas":canvas,"context":context});
 }
 
 //triggers event on every click on the  canvas
-/*
-canvas.addEventListener('click', function(event) {
+canvasClickListener.addEventListener('click', function(event) {
 	//getting position of the click on the canvas
 	const clickX = event.pageX - canvasX,
 		clickY = event.pageY - canvasY;
@@ -48,16 +49,17 @@ canvas.addEventListener('click', function(event) {
 	let clickedElement;
 	
 	//checks if the click is within one of the drawings
-	clickableElements.forEach(function(element) {
-		if ((clickY > parseInt(element.y) && clickY < parseInt(element.y) + parseInt(element.height)) && (clickX > parseInt(element.x) && clickX < parseInt(element.x) + parseInt(element.width))) {
-			clickedElement = element;
+	drawingList.forEach(function(element) {
+		if ((clickY > parseInt(element.drawing.y) && clickY < parseInt(element.drawing.y) + parseInt(element.drawing.height)) && (clickX > parseInt(element.drawing.x) && clickX < parseInt(element.drawing.x) + parseInt(element.drawing.width))) {
+			if(element.drawing.clickable === "1"){
+				clickedElement = element;
+			}
 		}
 	});
 	
 	if(clickedElement !== undefined){
 		console.log("clicked element:");
-		console.log(clickedElement);
+		console.log(clickedElement.drawing);
 	}
 	
 }, false);
-*/
